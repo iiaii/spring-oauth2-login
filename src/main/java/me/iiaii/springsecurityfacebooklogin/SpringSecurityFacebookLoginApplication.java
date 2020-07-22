9d5ae3abd523fafe0e2490c7eafae6e88669c934
최정헌 <iiaii@zuminternet.com>
2020-07-22 17:46:47 +0900

    github -> google

--- src/main/java/me/iiaii/springsecurityfacebooklogin/SpringSecurityFacebookLoginApplication.java
+++ src/main/java/me/iiaii/springsecurityfacebooklogin/SpringSecurityFacebookLoginApplication.java
@@ -2,6 +2,23 @@ package me.iiaii.springsecurityfacebooklogin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
 
 @SpringBootApplication
 public class SpringSecurityFacebookLoginApplication {
@@ -9,5 +26,39 @@ public class SpringSecurityFacebookLoginApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityFacebookLoginApplication.class, args);
 	}
-
+//
+//	@Bean
+//	public OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService(WebClient rest) {
+//		DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+//		return request -> {
+//			OAuth2User user = delegate.loadUser(request);
+//			if (!"github".equals(request.getClientRegistration().getRegistrationId())) {
+//				return user;
+//			}
+//
+//			OAuth2AuthorizedClient client = new OAuth2AuthorizedClient
+//					(request.getClientRegistration(), user.getName(), request.getAccessToken());
+//			String url = user.getAttribute("organizations_url");
+//			List<Map<String, Object>> orgs = rest
+//					.get().uri(url)
+//					.attributes(oauth2AuthorizedClient(client))
+//					.retrieve()
+//					.bodyToMono(List.class)
+//					.block();
+//
+//			if (orgs.stream().anyMatch(org -> "spring-projects".equals(org.get("login")))) {
+//				return user;
+//			}
+//
+//			throw new OAuth2AuthenticationException(new OAuth2Error("invalid_token", "Not in Spring Team", ""));
+//		};
+//	}
+//
+//	@Bean
+//	public WebClient rest(ClientRegistrationRepository clients, OAuth2AuthorizedClientRepository authz) {
+//		ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 =
+//				new ServletOAuth2AuthorizedClientExchangeFilterFunction(clients, authz);
+//		return WebClient.builder()
+//				.filter(oauth2).build();
+//	}
 }
--- src/main/java/me/iiaii/springsecurityfacebooklogin/controller/UserController.java
+++ src/main/java/me/iiaii/springsecurityfacebooklogin/controller/UserController.java
@@ -1,28 +1,45 @@
 package me.iiaii.springsecurityfacebooklogin.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Collections;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/user")
 public class UserController extends WebSecurityConfigurerAdapter {
 
-    @GetMapping
+
+    @GetMapping("/user")
     public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-        return Collections.singletonMap("name", principal.getAttribute("name"));
+//        return Collections.singletonMap("name", principal.getAttribute("name"));
+        return principal.getAttributes();
     }
+
+//    @GetMapping("/error")
+//    public String error() {
+//        String message = (String) request.getSession().getAttribute("error.message");
+//        request.getSession().removeAttribute("error.message");
+//        return message;
+//    }
 
 
     @Override
@@ -36,10 +53,23 @@ public class UserController extends WebSecurityConfigurerAdapter {
                 .logout(l -> l
                         .logoutSuccessUrl("/").permitAll()
                 )
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
                 .exceptionHandling(e -> e
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 )
-                .oauth2Login();
+                .oauth2Login()
+//                .oauth2Login(o -> o
+//                        .failureHandler((request, response, exception) -> {
+//                            request.getSession().setAttribute("error.message", exception.getMessage());
+//                            handler.onAuthenticationFailure(request, response, exception);
+//                        })
+//                );
+        ;
         // @formatter:on
     }
 }
+
+
+
